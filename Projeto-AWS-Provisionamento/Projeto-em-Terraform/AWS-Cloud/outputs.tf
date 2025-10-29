a87be967fdddb16afd925563112a52d66ab387e3
PierreRodrigues <92173419+PierreRodrigues@users.noreply.github.com>
2025-10-29 19:28:03 -0300

    Correções
    Feito algumas correções nas linhas de código
    
    Co-Authored-By: Maria Silveira <82678575+mayasrl@users.noreply.github.com>
    Co-Authored-By: Nando Cardoso <95889205+nandotrystan@users.noreply.github.com>

--- Projeto-AWS-Provisionamento/Projeto-em-Terraform/AWS-Cloud/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "load_balancer_dns" {
-  description = "DNS público do Load Balancer"
-  value       = aws_lb.web_alb.dns_name
-}
--- Projeto-AWS-Provisionamento/Projeto-em-Terraform/AWS-Cloud/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "key_pair_name" {
-  description = "Nome do par de chaves EC2 existente"
-  type        = string
-}
